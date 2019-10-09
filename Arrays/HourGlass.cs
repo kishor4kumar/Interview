7642dfc4dc28642081d3aa3eaf9184eb116389f1
kishor4kumar@yahoo.com <kishor4kumar@yahoo.com>
2019-10-10 00:53:36 +0530

    HourGlass problem added

--- Arrays/HourGlass.cs
+++ Arrays/HourGlass.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Arrays
 {
     public class HourGlass
     {
+        public static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for(int i=0; i<6; i++)
+            {
+                arr[i] = Array.ConvertAll(
+                            Console.ReadLine().Split(' '), 
+                            stringValue => Convert.ToInt32(stringValue));
+            }
+
+            Console.WriteLine(GetHourGlassMaxSum(arr));
+        }
+
+        public static int GetHourGlassMaxSum(int[][] arr)
+        {
+            var hourGlassSum = new List<int>();
+
+            for(int i=0; i<4; i++)
+            {
+                for(int j=0; j<4; j++)
+                {
+                    int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                                            arr[i + 1][j + 1] +
+                             arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                    hourGlassSum.Add(sum);
+                }
+            }
+
+            return hourGlassSum.Max();
+        }
     }
 }
--- ArraysTest/HourGlassTest.cs
+++ ArraysTest/HourGlassTest.cs
@@ -7,7 +7,17 @@ namespace ArraysTest
         [Test]
         public void BasicTest()
         {
-            Assert.Pass();
+            int[][] arr = new int[][]
+                {
+                   new int[]{ 1, 1, 1, 0, 0, 0 },
+                   new int[]{ 0, 1, 0, 0, 0, 0 },
+                   new int[]{ 1, 1, 1, 0, 0, 0 },
+                   new int[]{ 0, 0, 2, 4, 4, 0 },
+                   new int[]{ 0, 0, 0, 2, 0, 0 },
+                   new int[]{ 0, 0, 1, 2, 4, 0 }
+                };
+
+            Assert.AreEqual(19, Arrays.HourGlass.GetHourGlassMaxSum(arr));
         }
     }
 }
